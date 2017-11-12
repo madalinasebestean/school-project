ba76d067412c4e598263fb71d8bc38b8ed79699f
PomianAurelian <fruits_go_wild@yahoo.com>
2017-11-13 00:59:45 +0200

    repositories

--- src/main/java/repositories/FlightRepository.java
+++ src/main/java/repositories/FlightRepository.java
@@ -27,31 +27,33 @@ public class FlightRepository {
         return null;
     }
 
-    public User viewFlight(int id){
-        Flight user = new Flight();
+    public Flight viewFlight(int id){
+        Flight flight = new Flight();
 
         try {
             Connection con = this.getConnection();
             PreparedStatement x = con.prepareStatement("SELECT * FROM flights WHERE id = '" + id + "' ");
             ResultSet result = x.executeQuery();
-            while (result.next()) {
-                user = new User(result.getString("user"), result.getString("password"),
-                        result.getString("type"));
-            }
+            flight = new Flight(
+                result.getFlightNumber("flight_number"), result.getAirplaneType("password"),
+                result.getDepartureCity("departure_city_id"), result.getDepartureTime("departure_time"),
+                result.getArrivalCity("arrival_city_id"), result.getArrivalTime("arrival_time")
+            );
         } catch (Exception e) {
             System.out.println(e);
         }
 
-        return user;
+        return flight;
     }
 
     public void createFlight(Flight flight){
         try {
             Connection con = this.getConnection();
             PreparedStatement posted = con.prepareStatement(
-                "INSERT INTO flights (airplane_type, departure_city_id, departure_time, arrival_city_id, arrival_time) VALUES " +
-                    "( '" + flight.getAirplaneType() + "', '" + flight.getDepartureCity() + "', '" + flight.getDepartureTime() +
-                    "', '"f flight.getArrivalCity() + "', '" + flight.getArrivalTime() + "', '" + "');");
+                "INSERT INTO flights (flight_number, airplane_type, departure_city_id, departure_time, arrival_city_id, arrival_time) VALUES " +
+                "( '" + flight.getFlightNumber() + "', '" + flight.getAirplaneType() + "', '" + flight.getDepartureCity() + "', '" +
+                flight.getDepartureTime() + "', '"f flight.getArrivalCity() + "', '" + flight.getArrivalTime() +
+                "', '" + "');");
             posted.executeUpdate();
         } catch (Exception e) {
             System.out.println(e);
@@ -63,12 +65,13 @@ public class FlightRepository {
             Connection con = this.getConnection();
             Statement stm = con.createStatement();
             String sql = "UPDATE flights SET" +
-                    "airplane_type = '" + flight.getAirplaneType() + "' ," +
-                    "departure_city_id = '" + flight.getDepartureCity() + "' " +
-                    "departure_time = '" + flight.getDepartureTime() + "' " +
-                    "arrival_city_id = '" + flight.getArrivalCity() + "' " +
-                    "arrival_time = '" + flight.getArrivalTime() + "' " +
-                    "WHERE id = '" + flight.getFlightNumber() + "'";
+                "flight_number = '" + flight.getFlightNumber() + "' ," +
+                "airplane_type = '" + flight.getAirplaneType() + "' ," +
+                "departure_city_id = '" + flight.getDepartureCity() + "' " +
+                "departure_time = '" + flight.getDepartureTime() + "' " +
+                "arrival_city_id = '" + flight.getArrivalCity() + "' " +
+                "arrival_time = '" + flight.getArrivalTime() + "' " +
+                "WHERE id = '" + flight.getId() + "'";
             stm.executeUpdate(sql);
         } catch (Exception e) {
             System.out.println(e);
@@ -78,7 +81,7 @@ public class FlightRepository {
     public void deleteFlight(Flight flight){
         try {
             Connection con = this.getConnection();
-            PreparedStatement del = con.prepareStatement("DELETE FROM flights WHERE id = '" + user.getFlightNumber() + "' ");
+            PreparedStatement del = con.prepareStatement("DELETE FROM flights WHERE id = '" + user.getId() + "' ");
             del.executeUpdate();
         } catch (Exception e) {
             System.out.println(e);
